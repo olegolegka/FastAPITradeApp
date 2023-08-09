43d7edee8cf8ad187d200f90c5f61539404d2fe8
Oleg <oleg1babchenok1@gmail.com>
2023-08-10 00:21:17 +0300

    recreated database with docker-compose

--- migrations/env.py
+++ migrations/env.py
@@ -2,13 +2,13 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+
 from alembic import context
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 from models.models import metadata
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
 section = config.config_ini_section
 config.set_section_option(section, "DB_HOST", DB_HOST)
 config.set_section_option(section, "DB_PORT", DB_PORT)
